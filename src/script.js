48d355b9e1d105660ee864871640493bf636ba51
gremlinflat_ <fahri.119140205@student.itera.ac.id>
2021-10-16 13:32:14 +0700

    fix: handling negative number

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 var fact_log = [];
-function factorial(num){
+function factorial(num) {
+  if (num > 0) {
     fact_log = [1, 1];
-    let anchor=1;
-    for(let i = 2; i <= num; i++){
-        anchor = anchor * i;
-        fact_log.push(anchor);
+    let anchor = 1;
+    for (let i = 2; i <= num; i++) {
+      anchor = anchor * i;
+      fact_log.push(anchor);
     }
     return anchor;
+  } else {
+      return -1;
+  }
 }
-
